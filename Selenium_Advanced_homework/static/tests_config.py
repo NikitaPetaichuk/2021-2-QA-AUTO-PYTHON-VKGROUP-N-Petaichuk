a748cec53e6da498bd36658e2852c87432fb8a0b
NikitaPetaichuk <pet.ai.4.uk@yandex.ru>
2021-10-26 02:04:21 +0300

    Added picture for campaign creation and tests config file

---
+++ Selenium_Advanced_homework/static/tests_config.py
@@ -0,0 +1,16 @@
+class TestsConfig:
+
+    TEST_START_POINT_URL = 'https://target.my.com/'
+    INVALID_LOGIN_PAGE_URL_PREFIX = 'https://account.my.com/login/?error_code=1'
+
+    CLICK_ATTEMPTS_COUNT = 3
+    DEFAULT_WAITING_TIMEOUT = 10
+    SCREENSHOT_NAME = 'failure_state.png'
+    BROWSER_LOG_FILE_NAME = 'browser.log'
+    INTERNAL_LOG_FILE_NAME = 'tests.log'
+
+    LOGIN_EMAIL = 'pet.ai.4.uk@yandex.ru'
+    LOGIN_PASSWORD = 'TestAccount123'
+    TARGET_URL = 'mail.ru'
+    PICTURE_DIRECTORY = 'static'
+    PICTURE_NAME = 'banner.jpg'
