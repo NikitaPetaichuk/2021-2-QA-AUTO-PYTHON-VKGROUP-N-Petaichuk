3832e947a9b9824a33d01f3472083f63382745ae
NikitaPetaichuk <pet.ai.4.uk@yandex.ru>
2021-12-20 02:50:20 +0300

    Script for testing start modified

--- Final_project/start_testing_script.sh
+++ Final_project/start_testing_script.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-pytest -v -s --alluredir="${WORKSPACE}"/tests_logs/allure tests/
+cmd="pytest -v -s --alluredir=${WORKSPACE}/tests_logs/allure tests/"
+
+if [ -n "${TESTS_KEYWORD}" ]; then
+  cmd="$cmd -k ${TESTS_KEYWORD}"
+fi
+
+${cmd}
